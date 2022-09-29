d92d4ddf1923f977d936ab65cd1e59a6d18562f7
Kim tae hyun <83878234+Kim-taehyun-men@users.noreply.github.com>
2022-09-29 02:53:23 +0000

    01_special_rps

--- 01_special_rps/main.py
+++ 01_special_rps/main.py
@@ -1,7 +1,7 @@
 import random
 
-count_scissors = 0 
-count_rock = 0 
+count_scissors = 0
+count_rock = 0
 count_paper = 0
 
 print("특별한 가위바위보 게임")
@@ -9,11 +9,10 @@ print("사람 수를 입력하시오.")
 player_number = input()
 print(player_number)
 player_rpc = []
-for i in range(0,int(player_number)):
-  a = random.randrange(1,4) #가위=1, 바위=2, 보=3
+for i in range(0, int(player_number)):
+  a = random.randrange(1, 4)  #가위=1, 바위=2, 보=3
   player_rpc.append(a)
 
-  
 for i in player_rpc:
   result = player_rpc[i]
   print(result)
@@ -23,3 +22,4 @@ for i in player_rpc:
     count_rock += 1
   else:
     count_paper += 1
+sss
